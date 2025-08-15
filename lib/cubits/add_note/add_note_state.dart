f31822c8a0641eb6e0f9a9e6268eb7520a3dad46
mohamed hatem <mohm77778727@gmail.com>
2025-08-15 18:07:02 +0300

    add note state

--- lib/cubits/add_note/add_note_state.dart
+++ lib/cubits/add_note/add_note_state.dart
@@ -1,4 +1,13 @@
-class NoteState{
-  
-}
+class NoteState {}
 
+class InitialNoteState extends NoteState {}
+
+class LoadingNoteState extends NoteState {}
+
+class SuccessNoteState extends NoteState {}
+
+class FailureNoteState extends NoteState {
+  final String error;
+
+  FailureNoteState({required this.error});
+}
