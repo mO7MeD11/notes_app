35a1d40ada2b965960f02f44ded99f251601bf95
mohamed hatem <mohm77778727@gmail.com>
2025-08-18 14:22:21 +0300

    provide Notescubit to NotesView

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,29 +1,34 @@
+import 'package:app/cubits/notescubit/NotesCubit.dart';
 import 'package:app/widgets/Custom_Add_Note.dart';
 import 'package:app/widgets/custom_list_view.dart';
 import 'package:app/widgets/notes_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            context: context,
-            builder: (context) {
-              return CustomAddNote();
-            },
-          );
-        },
-        child: Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => Notescubit (),
+      child: Scaffold(
+        body: NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return CustomAddNote();
+              },
+            );
+          },
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
