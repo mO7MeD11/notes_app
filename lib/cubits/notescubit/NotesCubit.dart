07cc0e1c257ef941127dd905d0bf1cc489c18939
mohamed hatem <mohm77778727@gmail.com>
2025-08-18 14:18:02 +0300

    create getallnotes method

--- lib/cubits/notescubit/NotesCubit.dart
+++ lib/cubits/notescubit/NotesCubit.dart
@@ -1,10 +1,20 @@
+import 'package:app/const.dart';
+import 'package:app/cubits/add_note/add_note_state.dart';
+import 'package:app/cubits/notescubit/NotesState.dart';
+import 'package:app/models/note_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 
 class Notescubit extends Cubit {
-  Notescubit(super.initialState);
-  
-  
-  
-  
-  
+  Notescubit() : super(Notesstate);
+
+  getallnotes() async {
+    emit(NoteStateLoading());
+    try {
+      var notebox = Hive.box<NoteModel>(Knotesbox);
+      emit(NoteStateSuccess(notebox.values.toList()));
+    } on Exception catch (e) {
+      emit(NoteStateFailure(error: 'there was an error ${e.toString()}'));
+    }
+  }
 }
